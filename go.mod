4aebde86201294b102f80061629009eb73710579
Yerassyl Tleugazy <tleugazy98@gmail.com>
2022-02-04 01:03:10 +0600

    commits

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/kirigaikabuto/tik-lib
 go 1.13
 
 require (
-	github.com/kirigaikabuto/setdata-common v1.0.1-0.20211104130143-ab6a82248106 // indirect
+	github.com/google/uuid v1.1.2
+	github.com/kirigaikabuto/setdata-common v1.0.1-0.20211104130143-ab6a82248106
 	github.com/lib/pq v1.10.4
+	github.com/spf13/viper v1.10.1
 )
---
+++ user_test.go
@@ -0,0 +1,84 @@
+package tik_lib
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	setdata_common "github.com/kirigaikabuto/setdata-common"
+	"github.com/spf13/viper"
+	"log"
+	"os"
+	"testing"
+)
+
+var (
+	store UserStore
+)
+
+func init() {
+	filepath, err := os.Getwd()
+	if err != nil {
+		panic("main, get rootDir error" + err.Error())
+		return
+	}
+	viper.SetConfigName("main")
+	viper.AddConfigPath(filepath + "./config/")
+	err = viper.ReadInConfig()
+	if err != nil {
+		panic("main, fatal error while reading config file: " + err.Error())
+		return
+	}
+	cfg := PostgresConfig{
+		Host:     viper.GetString("db.primary.host"),
+		Port:     5432,
+		User:     viper.GetString("db.primary.user"),
+		Password: viper.GetString("db.primary.pass"),
+		Database: viper.GetString("db.primary.name"),
+		Params:   viper.GetString("db.primary.param"),
+	}
+	st, err := NewPostgreUserStore(cfg)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	store = st
+	//Db connection
+}
+
+func TestUserStore_Create(t *testing.T) {
+	filepath, err := os.Getwd()
+	if err != nil {
+		panic("main, get rootDir error" + err.Error())
+		return
+	}
+	viper.SetConfigName("user_create")
+	viper.AddConfigPath(filepath + "./config/")
+	err = viper.ReadInConfig()
+	if err != nil {
+		panic("main, fatal error while reading config file: " + err.Error())
+		return
+	}
+	hashPass, err := setdata_common.HashPassword(viper.GetString("user.password"))
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	userTest := &User{
+		Id:                  uuid.New().String(),
+		FirstName:           viper.GetString("user.first_name"),
+		LastName:            viper.GetString("user.last_name"),
+		Username:            viper.GetString("user.username"),
+		PhoneNumber:         viper.GetString("user.phone_number"),
+		Email:               viper.GetString("user.email"),
+		Password:            hashPass,
+		AvatarUrl:           viper.GetString("user.avatar_url"),
+		EmailVerified:       viper.GetBool("user.email_verified"),
+		PhoneNumberVerified: viper.GetBool("user.phone_number_verified"),
+		TypeOfUser:          ToUserType(viper.GetString("user.type_of_user")),
+	}
+	newUser, err := store.Create(userTest)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	fmt.Println("after user create", newUser)
+}
